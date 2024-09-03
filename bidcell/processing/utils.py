403797b041c9d41e62b524ef91dd2785fa148831
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2024-09-02 21:53:08 -0400

    Added image bit depth checker

--- bidcell/processing/utils.py
+++ bidcell/processing/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import multiprocessing as mp
-
+import warnings
 
 def get_patches_coords(size, size_patch):
     """Get start and end locations of patches in a large image given the image patch sizes"""
@@ -23,3 +23,12 @@ def get_n_processes(n_processes):
         return mp.cpu_count()
     else:
         return n_processes if n_processes <= mp.cpu_count() else mp.cpu_count()
+
+map_bit_depth = {np.uint64: np.uint16, np.uint32: np.uint16, np.int64: np.int16, np.int32: np.int16}
+def check_bit_depth(img, disallowed=map_bit_depth): 
+    for dtype_from, dtype_to in map_bit_depth.items():
+        if img.dtype == dtype_from:
+            warnings.warn(f"Image dtype was {img.dtype}, which is incompatible; converting to {dtype_to}")
+            img.astype(dtype_to)
+            break
+    return img
