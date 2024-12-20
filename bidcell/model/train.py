4b7f9677005dc7b814e6e0b69a6693cf78cb86d4
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2024-12-20 03:11:03 -0500

    Update train.py

--- bidcell/model/train.py
+++ bidcell/model/train.py
@@ -373,7 +373,7 @@ def train(config: Config, learning_rate = None, selected_solver = None):
         print("\nEpoch =", (epoch + 1), " lr =", cur_lr, " solver =", current_solver)
 
         for step_epoch, (
-            batch_expr_sum,
+            batch_ess,
             batch_x313,
             batch_n,
             batch_sa,
@@ -385,7 +385,7 @@ def train(config: Config, learning_rate = None, selected_solver = None):
             expr_aug_sum,
         ) in enumerate(train_loader):
             print(f"Original data shapes: ")
-            print(f"\tbatch_expr_sum shape: {batch_expr_sum.shape} | type: {type(batch_expr_sum)}")
+            print(f"\tbatch_ess shape: {batch_ess.shape} | type: {type(batch_ess)}")
             print(f"\tbatch_x313 shape = {batch_x313.shape} | type: {type(batch_x313)}")
             print(f"\tbatch_n shape = {batch_n.shape} | type: {type(batch_n)}")
             print(f"\tbatch_sa shape = {batch_sa.shape} | type: {type(batch_sa)}")
@@ -396,6 +396,7 @@ def train(config: Config, learning_rate = None, selected_solver = None):
             
             # Permute channels axis to batch axis
             batch_x313 = batch_x313[0, :, :, :, :].permute(3, 2, 0, 1)
+            batch_ess = batch_ess.permute(3, 0, 1, 2)
             batch_sa = batch_sa.permute(3, 0, 1, 2)
             batch_pos = batch_pos.permute(3, 0, 1, 2)
             batch_neg = batch_neg.permute(3, 0, 1, 2)
@@ -426,7 +427,7 @@ def train(config: Config, learning_rate = None, selected_solver = None):
             print(f"\texpr_aug_sum shape = {expr_aug_sum.shape} | type: {type(expr_aug_sum)}")
             
             # Transfer to GPU
-            batch_expr_sum = batch_expr_sum.to(device)
+            batch_ess = batch_ess.to(device)
             batch_x313 = batch_x313.to(device)
             batch_sa = batch_sa.to(device)
             batch_pos = batch_pos.to(device)
@@ -439,7 +440,7 @@ def train(config: Config, learning_rate = None, selected_solver = None):
             seg_pred = model(batch_x313)
 
             # Compute individual losses as appropriate
-            computed_losses = compute_losses(seg_pred, batch_n, batch_sa, batch_pos, batch_neg, batch_expr_sum, 
+            computed_losses = compute_losses(seg_pred, batch_n, batch_sa, batch_pos, batch_neg, expr_aug_sum, 
                                              weights, device, combine_ne_ov, combine_os_ov, combine_cc_pn, is_first_step)
             loss_ne, loss_os, loss_cc, loss_ov, loss_mu, loss_pn, loss_ne_ov, loss_os_ov, loss_cc_pn, weights = computed_losses
             
