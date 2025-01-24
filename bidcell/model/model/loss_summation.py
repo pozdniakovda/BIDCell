64c7a8ea9e2fb5f3d113e265dc6d0c10acfc16d3
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2025-01-23 21:02:55 -0500

    Create loss_summation.py

---
+++ bidcell/model/model/loss_summation.py
@@ -0,0 +1,45 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class STCHLoss(nn.Module):
+    """
+    Performs smoothed Tchebycheff scalarization (STCH) to sum a set of losses.
+    The summed loss can then be subjected to gradient descent. 
+    
+    Based on Lin et al. (2024) ICML2024 Paper: 
+    "Smooth Tchebycheff Scalarization for Multi-Objective Optimization"
+    """
+
+    def __init__(self, preference_weights, device) -> None:
+        super(STCHLoss, self).__init__()
+        self.preference_weights = preference_weights
+
+    def forward(self, losses, preference_weights=None, ideal_vals=None, mu=1.0):
+        # Preference weights are equivalent to loss weights in the config
+        if preference_weights is None:
+            preference_weights = self.preference_weights
+
+        # These are used to evaluate the  distance to an ideal loss value
+        if ideal_vals is None:
+            ideal_vals = np.zeros(len(losses), dtype=float)
+
+        # Create a list of STCH-scalarized losses to sum
+        stch_losses = []
+        for preference_weight, loss, ideal_val in zip(preference_weights, losses, ideal_vals):
+            stch_loss = loss - ideal_val # distance to idea value
+            stch_loss = stch_loss * preference_weight # applies the weight for this loss
+            stch_loss = stch_loss / mu # divides by a smoothing factor, usually between 0.5 and 2.0
+            stch_loss = torch.exp(stch_loss) # takes the exponential
+            stch_losses.append(stch_loss)
+
+        # Sum the STCH-scalarized loss terms
+        stch_losses = torch.stack(stch_losses)
+        stch_loss = torch.sum(stch_losses)
+
+        # Take the logarithm of the summed losses and scale by mu
+        stch_loss = torch.log(stch_loss)
+        stch_loss = stch_loss * mu
+
+        return stch_loss
