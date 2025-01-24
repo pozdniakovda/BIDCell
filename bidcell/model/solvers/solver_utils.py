c362be9542ce963a1bc7ee726f37d0c7f608541b
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2025-01-23 22:34:37 -0500

    Removed preference weights, which are pre-applied to losses

--- bidcell/model/solvers/solver_utils.py
+++ bidcell/model/solvers/solver_utils.py
@@ -41,74 +41,52 @@ def track_losses(tracked_losses, loss_ne = None, loss_os = None, loss_cc = None,
 
 def filter_non_contributing(loss_ne = None, loss_os = None, loss_cc = None, loss_ov = None, loss_mu = None, loss_pn = None, 
                             loss_ne_ov = None, loss_os_ov = None, loss_cc_pn = None, non_contributing_losses = (), 
-                            assign_none = False, preference_weights = {}): 
+                            assign_none = False): 
     # Remove non-contributing losses
     terms = [loss_ne, loss_os, loss_cc, loss_ov, loss_mu, loss_pn, loss_ne_ov, loss_os_ov, loss_cc_pn]
     keys = ["ne", "os", "cc", "ov", "mu", "pn", "ne_ov", "os_ov", "cc_pn"]
     
-    contributing_terms = {}
-    contributing_weights = {}
-    
-    blank_terms = {}
-    blank_weights = {}
-
-    spectator_terms = {}
-    spectator_weights = {}
+    contributing_losses = {}
+    blank_losses = {}
+    spectator_losses = {}
 
     for key, term in zip(keys, terms):
-        weight = preference_weights.get(key)
         if key in non_contributing_losses and term is not None:
-            spectator_terms[key] = term
-            spectator_weights[key] = weight
+            spectator_losses[key] = term
         elif key not in non_contributing_losses and term is not None:
-            contributing_terms[key] = term
-            contributing_weights[key] = weight
+            contributing_losses[key] = term
         else:
-            blank_terms[key] = term
-            blank_weights[key] = weight
+            blank_losses[key] = term
 
-    output = (contributing_terms, contributing_weights, 
-              blank_terms, blank_weights, 
-              spectator_terms, spectator_weights)
+    return contributing_losses, blank_losses, spectator_losses
 
-    return output
-
-def filter_unnecessary(contributing_terms, preference_weights = {}):
+def filter_unnecessary(contributing_losses):
     # Removes loss terms that are already covered by a combined loss term
     
-    keys = list(contributing_terms.keys())
+    keys = list(contributing_losses.keys())
     unnecessary_keys = []
     
-    necessary_terms = {}
-    necessary_weights = {}
-    
-    unnecessary_terms = {}
-    unnecessary_weights = {}
+    necessary_losses = {}    
+    unnecessary_losses = {}
 
     for key in keys:
         if "_" in key:
             unnecessary_keys.extend(key.split("_"))
     unnecessary_keys = list(tuple(unnecessary_keys))
 
-    for key, term in contributing_terms.items():
-        weight = preference_weights.get(key)
+    for key, term in contributing_losses.items():
         if key in unnecessary_keys:
-            unnecessary_terms[key] = term
-            unnecessary_weights[key] = weight
+            unnecessary_losses[key] = term
         else:
-            necessary_terms[key] = term
-            necessary_weights[key] = weight
+            necessary_losses[key] = term
 
-    return (necessary_terms, necessary_weights, unnecessary_terms, unnecessary_weights)
+    return necessary_losses, unnecessary_losses
 
 def filter_losses(optimizer, loss_ne = None, loss_os = None, loss_cc = None, loss_ov = None, loss_mu = None, 
                   loss_pn = None, loss_ne_ov = None, loss_os_ov = None, loss_cc_pn = None, 
-                  non_contributing_losses=(), preference_weights = None, squeeze = True):
+                  non_contributing_losses=(), squeeze = True):
     # Separates losses based on whether they should contribute to the summed loss
     
-    if preference_weights is None:
-        preference_weights = {key: 1.0 for key in ["ne", "os", "cc", "ov", "mu", "pn", "ne_ov", "os_ov", "cc_pn"]}
-
     if squeeze:
         loss_ne = loss_ne.squeeze() if loss_ne is not None else None
         loss_os = loss_os.squeeze() if loss_os is not None else None
@@ -121,27 +99,11 @@ def filter_losses(optimizer, loss_ne = None, loss_os = None, loss_cc = None, los
         loss_os_ov = loss_os_ov.squeeze() if loss_os_ov is not None else None
         loss_cc_pn = loss_cc_pn.squeeze() if loss_cc_pn is not None else None
 
-    # Filter losses
-
     # Filter based on whether a loss is designated as contributing
     contribution_args = filter_non_contributing(loss_ne, loss_os, loss_cc, loss_ov, loss_mu, loss_pn, 
                                                 loss_ne_ov, loss_os_ov, loss_cc_pn, 
-                                                non_contributing_losses, assign_none=False, 
-                                                preference_weights=preference_weights)
+                                                non_contributing_losses, assign_none=False)
+    contributing_losses, blank_losses, spectator_losses = contribution_args
+    contributing_losses, unnecessary_losses = filter_unnecessary(contributing_losses)
 
-    # Filter out unnecessary losses that are covered by another combined loss
-    contributing_terms, contributing_weights = contribution_args[:2]
-    necessity_args = filter_unnecessary(contributing_terms, preference_weights)
-    contributing_terms, contributing_weights = necessity_args[:2]
-    unnecessary_terms, unnecessary_weights = necessity_args[2:]
-
-    # Assemble dicts of loss terms and preference weights
-    filtered_terms, filtered_weights = {}, {}
-    
-    filtered_terms["contributing"], filtered_weights["contributing"] = necessity_args[:2]
-    filtered_terms["unnecessary"], filtered_weights["unnecessary"] = necessity_args[2:4]
-    
-    filtered_terms["blank"], filtered_weights["blank"] = contribution_args[2:4]
-    filtered_terms["spectator"], filtered_weights["spectator"] = contribution_args[4:6]
-
-    return filtered_terms, filtered_weights
+    return contributing_losses, unnecessary_losses, blank_losses, spectator_losses
