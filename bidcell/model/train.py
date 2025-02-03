1b2e0dbfec9d12f676fbe98dbd25a31419a0b7cf
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2025-02-02 20:00:22 -0500

    Update train.py

--- bidcell/model/train.py
+++ bidcell/model/train.py
@@ -376,9 +376,7 @@ def permute_channels(batch_ess, batch_x313, batch_n, batch_sa, batch_pos, batch_
     return permuted_data
 
 def save_loss_vals(csv_path, losses, ma_losses=None, col_prefix=None, col_suffix=None): 
-    # Saves loss values so they can be plotted elsewhere
-    print(f"Saving loss vals...")
-    
+    # Saves loss values so they can be plotted elsewhere    
     loss_data = {}
     for key in losses.keys():
         loss_col = f"{key}_values"
@@ -394,9 +392,6 @@ def save_loss_vals(csv_path, losses, ma_losses=None, col_prefix=None, col_suffix
         if ma_losses is not None:
             loss_data[ma_loss_col] = ma_losses[key]
 
-    for key, vals in loss_data.items():
-        print(f"\t{key}: len(vals) = {len(vals)}")
-    
     loss_df = pd.DataFrame(loss_data)
     loss_df.to_csv(csv_path)
 
