3166af072b447b7a0a8bab0fab3600fcbaaefdb2
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2025-02-10 20:21:26 -0500

    Update train.py

--- bidcell/model/train.py
+++ bidcell/model/train.py
@@ -622,7 +622,7 @@ def train(config: Config, learning_rate = None, selected_solver = None, device_i
                     patch_fp = os.path.join(patch_fp, f"epoch_{epoch+1}_{step_epoch}_{coords_h1}_{coords_w1}.png")
                     
                     save_fig_outputs(sample_seg, sample_n, sample_sa, sample_expr, patch_fp, random_seed=42, fig_count=10)
-                    logging.info(f"Saved sample outputs at epoch {epoch}, step {epoch_step}: {patch_fp}")
+                    logging.info(f"Saved sample outputs at epoch {epoch}, step {step_epoch}: {patch_fp}")
                     
                     print(f"Epoch[{epoch+1}/{total_epochs}], Step[{step_epoch}], Total Loss:{total_loss:.4f}")
     
