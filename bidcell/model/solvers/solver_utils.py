8e19c42e12946fb10cd6c2761c74190b526cf70d
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2025-02-20 21:20:57 -0500

    Replaced track_loss(es) with assign_loss(es)

--- bidcell/model/solvers/solver_utils.py
+++ bidcell/model/solvers/solver_utils.py
@@ -10,34 +10,51 @@ def to_scalar(value):
             print("Cannot apply .item() to a tensor with more than one element.")
     return value
 
-def track_loss(tracked_losses, key, loss_val):
-    if key not in tracked_losses.keys():
-        tracked_losses[key] = []
-    tracked_losses[key].append(loss_val)
+loss_key_conversion = {"ne": "Nuclei Encapsulation Loss", 
+                       "os": "Oversegmentation Loss", 
+                       "cc": "Cell Calling Loss", 
+                       "ov": "Overlap Loss", 
+                       "mu": "Multiple Assignment Loss", 
+                       "pn": "Pos-Neg Marker Loss", 
+                       "ne_ov": "Combined Nuclei Encapsulation and Overlap Loss", 
+                       "os_ov": "Combined Oversegmentation and Overlap Loss", 
+                       "cc_pn": "Combined Cell Calling and Marker Loss", 
+                       "total": "Total Loss"}
 
-def track_losses(tracked_losses, loss_ne = None, loss_os = None, loss_cc = None, loss_ov = None, loss_mu = None, 
-                 loss_pn = None, loss_ne_ov = None, loss_os_ov = None, loss_cc_pn = None, loss_total = None): 
-    # Track losses
-    if loss_ne is not None:
-        track_loss(tracked_losses, "Nuclei Encapsulation Loss", loss_ne)
-    if loss_os is not None:
-        track_loss(tracked_losses, "Oversegmentation Loss", loss_os)
-    if loss_cc is not None:
-        track_loss(tracked_losses, "Cell Calling Loss", loss_cc)
-    if loss_ov is not None:
-        track_loss(tracked_losses, "Overlap Loss", loss_ov)
-    if loss_mu is not None:
-        track_loss(tracked_losses, "Multiple Assignment Loss", loss_mu)
-    if loss_pn is not None:
-        track_loss(tracked_losses, "Pos-Neg Marker Loss", loss_pn)
-    if loss_ne_ov is not None:
-        track_loss(tracked_losses, "Combined Nuclei Encapsulation and Overlap Loss", loss_ne_ov)
-    if loss_os_ov is not None:
-        track_loss(tracked_losses, "Combined Oversegmentation and Overlap Loss", loss_os_ov)
-    if loss_cc_pn is not None:
-        track_loss(tracked_losses, "Combined Cell Calling and Marker Loss", loss_cc_pn)
-    if loss_total is not None:
-        track_loss(tracked_losses, "Total Loss", loss_total)
+def assign_loss(tracked_losses, short_key, loss_val, loss_key_conversion = loss_key_conversion):
+    long_key = loss_key_conversion[short_key]
+    if loss_val is not None: 
+        if key not in tracked_losses.keys():
+            tracked_losses[long_key] = []
+        tracked_losses[long_key].append(loss_val)
+
+def assign_losses(tracked_losses, contributing_losses, spectator_losses, unnecessary_losses, blank_losses, 
+                  total_loss, short_keys=None, detach=True):
+    # Track individual losses
+    if short_keys is None:
+        short_keys = ["ne", "os", "cc", "ov", "mu", "pn", "ne_ov", "os_ov", "cc_pn"]
+    
+    for key in short_keys:
+        if contributing_losses.get(key) is not None:
+            step_term_loss = contributing_losses[key]
+        elif spectator_losses.get(key) is not None:
+            step_term_loss = spectator_losses[key]
+        elif unnecessary_losses.get(key) is not None:
+            step_term_loss = unnecessary_losses[key]
+        elif blank_losses.get(key) is not None:
+            step_term_loss = blank_losses[key]
+        else:
+            step_term_loss = 0
+
+        if step_term_loss != 0: 
+            step_term_loss = step_term_loss.detach().cpu().numpy() if detach else to_scalar(step_term_loss)
+        
+        assign_loss(tracked_losses, key, step_term_loss)
+
+    step_total_loss = total_loss.detach().cpu().numpy() if detach else to_scalar(step_total_loss)
+    assign_loss(tracked_losses, "total", step_total_loss)
+
+    return step_total_loss
 
 def filter_non_contributing(loss_ne = None, loss_os = None, loss_cc = None, loss_ov = None, loss_mu = None, loss_pn = None, 
                             loss_ne_ov = None, loss_os_ov = None, loss_cc_pn = None, non_contributing_losses = (), 
