5a718e57c4d713ede23cdd9b8502e17ce7dd0a71
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2025-02-25 22:26:00 -0500

    Update predict.py

--- bidcell/model/predict.py
+++ bidcell/model/predict.py
@@ -32,26 +32,13 @@ def get_paths(config, make_new, learning_rate, dynamic_solvers, selected_solver=
               starting_solver=None, ending_solver=None, epochs_before_switch=0, training_repeats=1):
     # Get paths to models and outputs
 
-    timestamp = get_experiment_id(
+    exp_id = get_experiment_id(
         make_new,
         config.experiment_dirs.dir_id,
         config.files.data_dir,
     )
+    experiment_path = os.path.join(config.files.data_dir, "model_outputs", exp_id)
     
-    print(f"config.files.data_dir = {config.files.data_dir}")
-    print(f"config.experiment_dirs.dir_id = {config.experiment_dirs.dir_id}")
-    print(f"timestamp = {timestamp}")
-    print(f"learning_rate = {learning_rate}")
-    if not dynamic_solvers: 
-        print(f"selected_solver = {selected_solver}")
-        experiment_path = os.path.join(config.files.data_dir, "model_outputs", f"{timestamp}_{selected_solver}_lr-{learning_rate}")
-    else:
-        print(f"starting_solver = {starting_solver}")
-        print(f"ending_solver = {ending_solver}")
-        print(f"epochs_before_switch = {epochs_before_switch}")
-        experiment_path = os.path.join(config.files.data_dir, "model_outputs", f"{timestamp}_{starting_solver}-to-{ending_solver}_switched-after-{epochs_before_switch}-epochs_lr-{learning_rate}")
-    print(f"experiment_path (dynamic_solvers={dynamic_solvers}) = {experiment_path}")
-
     if training_repeats > 1: 
         # Paths for each repeat
         repeat_paths = {}
