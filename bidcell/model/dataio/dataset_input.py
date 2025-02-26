7edb7439752027d4f0af0f6a872b8322ffdf2774
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2025-02-25 22:40:57 -0500

    Update dataset_input.py

--- bidcell/model/dataio/dataset_input.py
+++ bidcell/model/dataio/dataset_input.py
@@ -312,7 +312,8 @@ class DataProcessing(data.Dataset):
                         nucl_split[:, :, i_cell], kernel, iterations=1
                     )
 
-            c_id = int(c_id)
+            print("c_id =", c_id)
+            print("nuclei_types_ids =", self.nuclei_types_ids)
             ct_nucleus = int(self.nuclei_types_idx[self.nuclei_types_ids.index(c_id)])
             ct_nucleus_name = self.type_names[ct_nucleus]
 
