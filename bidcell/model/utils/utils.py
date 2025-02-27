c8dde4dcea037af45f4645cda258a1b9212c0639
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2025-02-26 21:05:50 -0500

    Update utils.py

--- bidcell/model/utils/utils.py
+++ bidcell/model/utils/utils.py
@@ -157,8 +157,9 @@ def save_fig_outputs(sample_seg, sample_n, sample_sa, sample_expr, patch_fp,
         if sample_expr.max() > 0:
             sample_expr = sample_expr / sample_expr.max()
 
-    print(f"sample_expr has {np.unique(sample_expr)} unique values; "
-          f"min={sample_expr.min()}, max={sample_expr.max()}, mean={sample_expr.mean()}")
+    print(f"sample_expr has {len(np.unique(sample_expr))} unique values; "
+          f"min={sample_expr.min()}, max={sample_expr.max()}, "
+          f"mean={sample_expr.mean()}, median={np.median(sample_expr)}")
 
     # Get unique cell IDs
     cells_ids_orig = np.unique(final_seg_orig)
