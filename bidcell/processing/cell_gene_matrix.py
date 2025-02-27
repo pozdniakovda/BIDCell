40200eae7a6304f3daab3d42e63aa4cbb54f2e4c
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2025-02-26 22:38:05 -0500

    Update cell_gene_matrix.py

--- bidcell/processing/cell_gene_matrix.py
+++ bidcell/processing/cell_gene_matrix.py
@@ -66,6 +66,7 @@ def process_chunk(
 ):
     """Extract cell expression profiles"""
 
+    t1=time.time()
     df_out = pd.DataFrame(0, index=cell_ids_unique, columns=col_names)
     df_out["cell_id"] = cell_ids_unique.copy()
 
@@ -81,6 +82,10 @@ def process_chunk(
             df_out.loc[seg_val, gene] += 1
 
     df_out.to_csv(output_dir + "/" + "chunk_%d.csv" % chunk_id)
+
+    t2=time.time()
+    t=t2-t1
+    print(f"Processed cell gene matrix df in {t:.3f} seconds.")
 
 
 def process_chunk_meta(
