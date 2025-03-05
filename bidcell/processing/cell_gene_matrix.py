607383a274fbdcdbf839df972f10d486e51c69c6
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2025-03-05 01:12:11 -0500

    Update cell_gene_matrix.py

--- bidcell/processing/cell_gene_matrix.py
+++ bidcell/processing/cell_gene_matrix.py
@@ -4,10 +4,7 @@ import os
 import sys
 import time
 import multiprocessing as mp
-#mp.set_start_method("spawn", force=True)
-
-import ctypes
-ctypes.CDLL("libX11.so").XInitThreads()
+mp.set_start_method("forkserver", force=True)
 
 # import cv2
 from skimage.transform import resize
