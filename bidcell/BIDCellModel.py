9a585941f6e435f926e34635b7a2012e72f4cc4f
pozdniakovda <82898041+pozdniakovda@users.noreply.github.com>
2025-03-06 01:33:07 -0500

    Run preannotate() before make_cell_gene_mat()

--- bidcell/BIDCellModel.py
+++ bidcell/BIDCellModel.py
@@ -193,12 +193,12 @@ class BIDCellModel:
     
         print(f"Postprocessing predictions...")
         postprocess_predictions(self.config, timestamp)
-    
-        print(f"Making cell gene matrix...")
-        make_cell_gene_mat(self.config, is_cell=True, timestamp=timestamp)
-    
+
         print(f"Re-running preannotation...")
         preannotate(self.config, save_merged=True)
+
+        print(f"Making cell gene matrix...")
+        make_cell_gene_mat(self.config, is_cell=True, timestamp=timestamp)
         
         print(f"Reloading cell gene matrix to include annotations...")
         expr_mat_path = os.path.join(self.config.files.data_dir, self.config.files.dir_cgm, timestamp, self.config.files.fp_expr)
